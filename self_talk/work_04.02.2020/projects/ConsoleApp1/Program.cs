c7a72c44744ad43935de6a141d7ced80d835714f
Jeja <makushkinev34@gmail.com>
2020-02-20 15:33:11 +0400

    Work classes and class vector

---
+++ self_talk/work_04.02.2020/projects/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3D vector1 = new Vector3D(2, 4, 6);
+            Vector3D vector2 = new Vector3D(3, 6, 9);
+
+            Console.WriteLine($"вектор 1: {vector1.ToString()}");
+            Console.WriteLine($"вектор 2: {vector2.ToString()}");
+
+            Console.WriteLine($"Длина 1 вектора: {vector1.VectorLength}");
+            Console.WriteLine($"Длина 2 вектора: {vector2.VectorLength}");
+
+            Console.WriteLine($"Скалярное произведение: {vector1.ScalarProduct(vector2)}");
+            Console.WriteLine($"Векторное произведение: {(vector1.VectorProduct(vector2)).ToString()}");
+        }
+    }
+}
---
+++ self_talk/work_07.02.2020/projects/ClassVector/Vector3D.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vector3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public double VectorLength
+        {
+            get => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
+
+        public double ScalarProduct(Vector3D guestVector)
+        {
+            return ScalarProduct(guestVector.X, guestVector.Y, guestVector.Z);
+        }
+
+        public double ScalarProduct(double xGuest, double yGuest, double zGuest)
+        {
+            return X * xGuest + Y * yGuest + Z * zGuest;
+        }
+
+        public Vector3D VectorProduct(Vector3D guestVector)
+        {
+            return VectorProduct(guestVector.X, guestVector.Y, guestVector.Z);
+        }
+
+        public Vector3D VectorProduct(double xGuest, double yGuest, double zGuest)
+        {
+            Vector3D result = new Vector3D();
+            result.X = Y * zGuest - Z - yGuest;
+            result.Y = Z * xGuest - X * zGuest;
+            result.Z = X * yGuest - Y * xGuest;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"vec (x: {X}; y: {Y}; z: {Z})";
+        }
+
+        public Vector3D(double x = 0, double y = 0, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
